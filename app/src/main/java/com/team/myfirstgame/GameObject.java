499171967bfcddebd2656e258b415e92ae16ccb3
root <root@localhost.localdomain>
2015-05-25 19:11:07 +0200

    Initial commit

---
+++ app/src/main/java/com/team/myfirstgame/GameObject.java
@@ -0,0 +1,45 @@
+package com.team.myfirstgame;
+
+/**
+ * Created by xxxx on 2015-05-25.
+ */
+import android.graphics.Rect;
+
+public abstract class GameObject {
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getWidth()
+    {
+        return width;
+    }
+    public Rect getRectangle()
+    {
+        return new Rect(x, y, x+width, y+height);
+    }
+
+}
